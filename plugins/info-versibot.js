26129f630d9e9715f529d3b9debed6f8e34232e7
khairil <80386217+khairil8@users.noreply.github.com>
2023-01-10 21:50:40 +0800

    Update info-versibot.js

--- plugins/info-versibot.js
+++ plugins/info-versibot.js
@@ -8,7 +8,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
     const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
     const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
     const detek = Math.floor( Kurang % (1000 * 60) / 1000)
-  let info = `Bot ini menggunakan script by\n ᴹᴿ᭄ PutraModz ×፝֟͜×\n\n*Version:* Private Edition V4`
+  let info = `Bot ini menggunakan script by\n BebanBot愛\n\n*Version:* Private Edition V1`
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 const message = {
@@ -20,13 +20,13 @@ const message = {
                 {
                     urlButton: {
                         displayText: 'Beli Sc? Klik',
-                        url: 'https://wa.me/6282214729677'
+                        url: 'https://wa.me/60109102984'
                     }
                 },
                 {
                     urlButton: {
                         displayText: 'Ke IG GW',
-                        url: 'https://Instragram.com/l4c1f2r',
+                        url: 'https://Instragram.com/coglah_07',
                     }
                 },
             ]
@@ -34,13 +34,13 @@ const message = {
         conn.sendMessage(m.chat, message)
 conn.reply(m.chat, info, m, {
 contextInfo: { externalAdReply :{ showAdAttribution: true,
-                        sourceUrl: 'https://youtu.be/ALQiRR1nzLI',
+                        sourceUrl: 'https://youtu.be/n5i3QRvPA9Y',
                         mediaType: 2,
-                        description: `⚘ ᴄʀᴇᴀᴛᴇ ᴹᴿ᭄ PutraModz ×፝֟͜×`,
+                        description: `⚘ ᴄʀᴇᴀᴛᴇ Khairil春,
                         title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
-                        body: `⚘ ᴄʀᴇᴀᴛᴇ by ᴹᴿ᭄ PutraModz ×፝֟͜×`,          previewType: 0,
+                        body: `⚘ ᴄʀᴇᴀᴛᴇ by ᴹᴿ᭄ Khairil春×`,          previewType: 0,
                         thumbnail: await (await fetch(giflogo2)).buffer(),
-                        mediaUrl: 'https://youtu.be/ALQiRR1nzLI'
+                        mediaUrl: 'https://youtu.be/n5i3QRvPA9Y'
                         
                       }}
 })

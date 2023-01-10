5d276cca9fc4f8680e97b549c6d499a37d3c8e83
khairil <80386217+khairil8@users.noreply.github.com>
2023-01-10 22:09:27 +0800

    Update woi.js

--- plugins/woi.js
+++ plugins/woi.js
@@ -32,10 +32,10 @@ contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
-                        title: `A PUTRA🎧 : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        title: `Khairil春🎧 : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
                         body: `Join Grup`,          previewType: 0,
                         thumbnail: await (await fetch(`https://i.ibb.co/5st560k/fake.jpg`)).buffer(),
-                        sourceUrl: 'https://chat.whatsapp.com/FR1KfE3OTcQCayiabos8Ff'
+                        sourceUrl: 'https://chat.whatsapp.com/Bn7PxZ06B3GL8BDEZhC36T'
             }
         }
     })
